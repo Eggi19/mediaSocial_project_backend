ce520be615d95e79c859e02c3b422c9913f84a89
Eggi19 <eggiyapari19@gmail.com>
2023-06-16 14:20:35 +0800

    master

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,12 +1,63 @@
+const { get } = require('../Helpers/transporter')
 const db = require('../models')
 const User = db.User
+const bcrypt = require('bcrypt')
 
 module.exports = {
-    login: (req, res) => {
+    login: async (req, res) => {
         try {
-            
+            const { usernameOrEmail, password } = req.body
+            const isEmail = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}')
+
+            if (isEmail.test(usernameOrEmail)) {
+                const getUser = await User.findOne({
+                    where: {
+                        email: usernameOrEmail
+                    }
+                })
+
+                if (getUser) {
+                    const isValid = await bcrypt.compare(password, getUser.password)
+                    if (isValid) {
+                        res.send({
+                            success: true,
+                            message: "Login Success",
+                            data: getUser
+                        })
+                    } else {
+                        throw { message: "Incorrect Password" }
+                    }
+                } else {
+                    throw { message: "account is not found" }
+                }
+            } else {
+                const getUser = await User.findOne({
+                    where: {
+                        username: usernameOrEmail
+                    }
+                })
+
+                if (getUser) {
+                    const isValid = await bcrypt.compare(password, getUser.password)
+                    if (isValid) {
+                        res.send({
+                            success: true,
+                            message: "Login Success",
+                            data: getUser
+                        })
+                    } else {
+                        throw { message: "Incorrect Password" }
+                    }
+                } else {
+                    throw { message: "account is not found" }
+                }
+            }
         } catch (error) {
-            
+            res.send({
+                success: false,
+                message: error.message,
+                data: null
+            })
         }
     }
 }
